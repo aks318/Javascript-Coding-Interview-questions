2ade34828e2fb7a9b975236bcd6647606fc53ac3
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-03-28 20:45:02 +0530

    maxChar from string

--- script.js
+++ script.js
@@ -11,14 +11,57 @@
 
 //====================================Palindrome============================================
 
-const Palindrome = (str) => {
-    let str2 = str.split('').reverse().join('')
-    console.log(str === str2)
+// const Palindrome = (str) => {
+//     let str2 = str.split('').reverse().join('')
+//     console.log(str === str2)
+// }
+
+// Palindrome("Akash")
+// Palindrome("AakaA")
+
+
+// ================================Maximum Character from String=======================================
+
+// const maxChar = (str) =>{
+//     let obj = {}
+//     for (let i of str){
+//         (!obj[i]) ? obj[i] = 1 : obj[i]++
+//     }
+//     let max = 0
+//     let maxChar = ""
+
+//     for(let i in obj){
+//         if(obj[i] >= max){
+//             max = obj[i]
+//             maxChar = i
+//         }
+//     }
+
+//     console.log(maxChar , max)
+// }
+
+// maxChar("Aakaahsksjaaljsa")
+
+const maxChar = (str) =>{
+    let obj = {}
+    let max = 1
+    let maxChar = str[0]
+    for (let i of str){
+        if(obj[i]){
+            if(obj[i] >= max){
+                max++
+            }
+            maxChar = i
+            obj[i]++
+        }
+        else{
+            obj[i] =1
+        }
+    }
+
+    console.log(maxChar , max)
 }
 
-Palindrome("Akash")
-Palindrome("AakaA")
-
-
+maxChar("AakaahsksjaaljsaAaAAAAAAAAAAAAAA")
 
 
