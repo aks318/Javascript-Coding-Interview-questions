bcda61aaf1913ede3acc07633f497b76d7d0aee5
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-03-28 20:18:14 +0530

    Palindrome Problem

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // Reverse a string
 
-let str = "Aakash"
-let reverse = ""
+// let str = "Aakash"
+// let reverse = ""
 
-for(let char of str){
-    reverse = char + reverse
+// for(let char of str){
+//     reverse = char + reverse
+// }
+
+// console.log(reverse)
+
+//====================================Palindrome============================================
+
+const Palindrome = (str) => {
+    let str2 = str.split('').reverse().join('')
+    console.log(str === str2)
 }
 
-console.log(reverse)
-
-
+Palindrome("Akash")
+Palindrome("AakaA")
 
 
 
