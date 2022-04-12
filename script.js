c8ffefba87f2f090cf31eb48779cc9c6c1110f5a
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-04-12 20:37:30 +0530

    Reverse Pyramid Pattern

--- script.js
+++ script.js
@@ -536,14 +536,37 @@
 //   *******
 //  *********
 
+// let n=5
+// let string=""
+
+// for(let i=0 ; i<n ; i++){
+//     for(let j=0 ; j<n-i-1; j++){
+//         string+=" "
+//     }
+//     for(k=0 ; k<2*i+1 ; k++){
+//         string+="*"
+//     }
+//     string+="\n"
+// }
+// console.log(string)
+
+
+// ===================================================================
+
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+
 let n=5
 let string=""
 
 for(let i=0 ; i<n ; i++){
-    for(let j=0 ; j<n-i-1; j++){
+    for(let j=0 ; j<i ; j++){
         string+=" "
     }
-    for(k=0 ; k<2*i+1 ; k++){
+    for(k=0 ; k<(n-i)*2 -1 ; k++){
         string+="*"
     }
     string+="\n"
